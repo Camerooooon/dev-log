5bed1bc265adcd52ba60a4dd8c33f4dc0227873a
Camerooooon <cjpuffnstuff@gmail.com>
2022-05-16 18:41:56 -0700

    Add log incrementation and titling

--- log.sh
+++ log.sh
@@ -1,16 +1,24 @@
 LOGS_DIRECTORY="/home/cameron/Repos/dev-log/logs/"
-
+DATE=$(date +%F)
 TIME=$(date +%F-%H:%M)
+
 echo Creating new entry for $TIME
+
 cd $LOGS_DIRECTORY
-touch $TIME.md
-echo "Log #$(ls | wc -l) | $TIME | T" > $TIME.md
-vim $TIME.md
-git add $TIME.md
+touch $DATE.md
+
+echo $(expr "$(cat count)" + 1) > count
+COUNT="$(cat count)"
+
+echo "Log #$COUNT | $TIME | " >> $DATE.md
+v "+normal G$" +startinsert $DATE.md
+
 read -p "Commit and push to the internet?" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     exit 1
 fi
-git commit -m "New log $TIME"
+
+git add $DATE.md
+git commit -m "New log $DATE.md #$COUNT"
 git push origin main
